542923057ddf6ed37c4a28d7e59b5c3d012bc262
Etrickery <ericspfister@gmail.com>
2015-04-02 21:14:04 -0400

    first few lines of code, testing operational

--- QuoteViewer/Resources/app.js
+++ QuoteViewer/Resources/app.js
@@ -1,1 +1,13 @@
 //Eric Pfister
+//Quote Viewer app
+//
+
+Ti.UI.setBackground('#000');
+
+var mainWindow = Ti.UI.createWindow({
+	backgroundColor: '#fff',
+	backgroundImage: 'patterns.png',
+	backgroundRepeat: true,
+	title: 'My first window'
+	
+});
