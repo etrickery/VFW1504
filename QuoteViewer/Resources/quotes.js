dadb6a04fc84a4a7a646a3058dc6148aae8f9c78
Etrickery <ericspfister@gmail.com>
2015-04-02 23:16:59 -0400

    label testing

---
+++ QuoteViewer/Resources/quotes.js
@@ -0,0 +1,17 @@
+//quote array
+
+var famousQuotes = [];
+
+famousQuotes[1] = "";
+famousQuotes[2] = "";
+famousQuotes[3] = "";
+famousQuotes[4] = "";
+famousQuotes[5] = "";
+famousQuotes[6] = "";
+famousQuotes[7] = "";
+famousQuotes[8] = "";
+famousQuotes[9] = "";
+famousQuotes[10] = "";
+
+
+
