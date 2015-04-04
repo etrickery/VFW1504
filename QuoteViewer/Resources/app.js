84b75cf6a64ea87cbe61ef465f13b3fb7f89ffc2
Etrickery <ericspfister@gmail.com>
2015-04-04 12:03:12 -0400

    bunch more stuff added

--- QuoteViewer/Resources/app.js
+++ QuoteViewer/Resources/app.js
@@ -4,6 +4,9 @@
 
 Ti.UI.setBackground('#000');
 
+var quoteList;
+var q;
+
 //Define main window
 var mainWindow = Ti.UI.createWindow({
 	backgroundColor: '#fff',
@@ -34,6 +37,7 @@ var titleText = Ti.UI.createLabel({
 });
 
 
+
 //Draw quote window
 var quoteView = Ti.UI.createView({
 	backgroundColor: '#888',
@@ -43,6 +47,20 @@ var quoteView = Ti.UI.createView({
 	top: '10%',
 	height: '80%',
 	width: '90%'
+});
+
+//quote text
+var quoteText = Ti.UI.createLabel({
+	color: '#fff',
+	font: {
+		fontSize: 30, 
+		fontFamily: 'Helvetica'
+	},
+	top: '5',
+	left: '5',
+	right: '5',
+	align: 'left',
+	text: "Quote Viewer"
 });
 
 //Draw previous nav button
@@ -101,13 +119,97 @@ var next = Ti.UI.createLabel({
 	
 });
 
-var quoteContent;
-var loadContent = require(content.js);
+//counter variable
+var q = "0";
+
+//Quote list array
+var quoteList = [];
 
 
+//define quoteText
+var quoteText;
+/*
+//nextQuote function
+var nextQuote = function(){
+	if (q < quoteList.length) {
+		quoteView.hide();
+		quoteView = null;
+		for(i = quoteList.length; q<i; q++){
+			quoteView = Ti.UI.createView({
+				backgroundColor: '#888',
+				borderRadius: 5,
+				borderWidth: 1,
+				borderColor: '#fff',
+				top: '10%',
+				height: '80%',
+				width: '90%'
+			});
+			quoteText = Ti.UI.createLabel({
+				color: '#fff',
+				font: {
+					fontSize: 30, 
+					fontFamily: 'Helvetica'
+				},
+				top: '5',
+				left: '5',
+				right: '5',
+				align: 'left',
+				text: quoteList[q]
+			});
+			mainWindow.add(quoteView);
+			quoteView.add(quoteText);
+		};
+	} else {
+		q=0;
+	}
+};
+
+//prevQuote function
+var prevQuote = function(){
+	if (q > 0){
+		quoteView.hide();
+		quoteView = null;
+		for(; q>-1; q--){
+			quoteView = Ti.UI.createView({
+				backgroundColor: '#888',
+				borderRadius: 5,
+				borderWidth: 1,
+				borderColor: '#fff',
+				top: '10%',
+				height: '80%',
+				width: '90%'
+			});
+			quoteText = Ti.UI.createLabel({
+				color: '#fff',
+				font: {
+					fontSize: 30, 
+					fontFamily: 'Helvetica'
+				},
+				top: '5',
+				left: '5',
+				right: '5',
+				align: 'left',
+				text: quoteList[q]
+			});
+			mainWindow.add(quoteView);
+			quoteView.add(quoteText);
+			
+		};
+	} else {
+		q = quoteList.length;
+		
+	}
+};
+
+var loadContent = require('content');
+
+//Event Listeners
+nextQuoteNav.addEventListener("click", nextQuote);
+prevQuoteNav.addEventListener("click", prevQuote);
+
+*/
 mainWindow.open();
 mainWindow.add(titleBar, quoteView, prevQuoteNav, nextQuoteNav);
-quoteView.add(quoteContent);
 prevQuoteNav.add(prev);
 nextQuoteNav.add(next);
 titleBar.add(titleText);
--- QuoteViewer/Resources/content.js
+++ QuoteViewer/Resources/content.js
@@ -1,17 +1,8 @@
-var quoteContent = Ti.UI.createLabel({
-	text: "Quote",
-	color: '#fff',
-	font: {
-		fontSize: 30, 
-		fontFamily: 'Helvetica', 
-		fontStyle: 'Italic'
-	},
-	top: '5',
-	left: '5',
-	right: '5',
-	align: 'left'	
+quoteList = 
+	["Say what you mean, mean what you say, and don't say it mean. - unknown", 
+	"Don't cry because it's over, smile because it happened. - Dr.Suess", 
+	"Be yourself; everyone else is already taken. - Oscar Wilde", 
+	"Two things are infinite: the universe and human stupidity; and I'm not sure about the Universe - Albert Einstein",
+	"Be the change that you wish to see in the world - Mahatma Gandhi"];
 	
-});
-
-
 
