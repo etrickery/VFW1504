0672e6af32ae938debd41a103fe4b90ed5b29995
Etrickery <ericspfister@gmail.com>
2015-04-04 00:54:45 -0400

    cleaned up and trying over

--- QuoteViewer/Resources/app.js
+++ QuoteViewer/Resources/app.js
@@ -4,60 +4,15 @@
 
 Ti.UI.setBackground('#000');
 
-
+//Define main window
 var mainWindow = Ti.UI.createWindow({
 	backgroundColor: '#fff',
 	backgroundImage: 'pattern.png',
 	backgroundRepeat: true,
-	title: 'My first window'
-	
-});
-
-var quoteView = Ti.UI.createView({	
-	backgroundColor: '#CCC',
-	borderRadius: 5,
-	borderWidth: 1,
-	borderColor: '#fff',
-	top: 20,
-	height: '80%',
-	width: '90%',
-	id: 'QuoteBox'
-});
-
-var quoteNav = Ti.UI.createView({
-	backgroundColor: '#888',
-	borderRadius: 5,
-	borderWidth: 1,
-	borderColor: '#fff',
-	top: '85%',
-	height: '10%',
-	width: '90%',
-	id: 'NavBox'
-});
-
-var quoteText = Ti.UI.createLabel({
-	text: 'Quote test',
-	color: '#fff',
-	font: {
-		fontSize: 22, 
-		fontFamily: 'Baskerville', 
-		fontWeight: 'Bold',
-		fontStyle: 'italic'
-	},
-	top: '5%',
-	left: '5%',
-	right: '5%'
-	
-	
+	title: 'Quote Viewer'
 });
 
 
-
-
-quoteView.add(quoteText);
-mainWindow.add(quoteNav);
-mainWindow.add(quoteView);
 mainWindow.open();
-
-
+var loadContent = require(content);
 
